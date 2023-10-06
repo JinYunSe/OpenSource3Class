54124d767323d13de4153d121d5abb23ab9d9f39
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2023-10-06 16:42:40 +0900

    이전에 보냈던 부분에 약간의 수정

--- Assets/Scripts/SceneLoadScript.cs
+++ Assets/Scripts/SceneLoadScript.cs
@@ -8,7 +8,8 @@ public class SceneLoadScript : MonoBehaviour
 	[SerializeField]
 	private Text nickName;
 	public void LoadScene(int idx)
-	{ 
+	{
+		if (nickName.text == null) return;
 		PhotonHandler.Instance.nickName = nickName.text;
         SceneManager.LoadScene(idx);
 		Debug.Log("접속한 방 : "+idx);
