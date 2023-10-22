2f843fae74cc24cd51c06893cbdf934578f63d2c
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2023-10-22 23:29:49 +0900

    꼬리 잡기 맵 만들기 시작
    맵 만들기 시작

--- Assets/Maker_JinYunSe/Scripts/PlaneRotation.cs
+++ Assets/Maker_JinYunSe/Scripts/PlaneRotation.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class PlaneRotation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 3f;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0f, speed * Time.deltaTime / 0.01f, 0f, Space.Self);
     }
+
 }
