930adf1b9978c4a0315a4a7eee4db1c0b11b48bb
JinYunSe <wlsdbstp1234@gmail.com>
2023-11-09 09:45:04 +0900

    누락 부분
    누락 부분 추가

--- Assets/ObstacleCoursePack/Scripts/Bounce.cs
+++ Assets/ObstacleCoursePack/Scripts/Bounce.cs
@@ -10,4 +10,18 @@ public class Bounce : MonoBehaviour
 	public float force = 10f; //Force 10000f
 	public float stunTime = 0.5f;
 	private Vector3 hitDir;
+
+	void OnCollisionEnter(Collision collision)
+	{
+		foreach (ContactPoint contact in collision.contacts)
+		{
+			if (collision.gameObject.tag == "Player")
+			{
+				hitDir = contact.normal;
+                Debug.Log(PhotonNetwork.LocalPlayer.NickName + " : " + hitDir);
+                collision.gameObject.GetComponent<CharacterControls>().HitPlayer(-hitDir * force, stunTime);
+                return;
+			}
+		}
+	}
 }
