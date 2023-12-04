7ecfa8b7714e05adccd764dbc41ba423387f9cad
jindayeong <jindy0423@gmail.com>
2023-12-04 23:25:18 +0900

    랭킹 스크립트 수정
    게임 끝난 후 점수 표시

--- Assets/Maker_JinDaYeong/Scripts/Ranking.cs
+++ Assets/Maker_JinDaYeong/Scripts/Ranking.cs
@@ -2,52 +2,65 @@ using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Ranking : MonoBehaviour, IPunObservable
 {
+    private TextMeshProUGUI rankText;
     private Dictionary<string, int> playerScores = new Dictionary<string, int>();
     private int count = 1;
 
-    // Start is called before the first frame update
     void Start()
     {
-        // 게임 시작 시 플레이어들의 점수 초기화
-        //InitializePlayerScores();
+        rankText = GameObject.Find("Rank").GetComponent<TextMeshProUGUI>();
         StartCoroutine(InitializePlayerScores());
     }
 
     private IEnumerator InitializePlayerScores()
     {
-        yield return null; 
+        yield return null;
 
-        Debug.Log(PhotonNetwork.CurrentRoom.Players.Count+"명");
+        Debug.Log(PhotonNetwork.CurrentRoom.Players.Count + "명");
 
-        // 플레이어 점수 초기값 설정
+        //플레이어 점수 초기값 설정
         foreach (var player in PhotonNetwork.CurrentRoom.Players)
         {
             string playerName = player.Value.NickName;
-            Debug.Log(playerName + " 입니다");
+            Debug.Log(playerName + "입니다.");
             playerScores[playerName] = 0;
         }
     }
+
     private void OnTriggerEnter(Collider other)
     {
         string name = other.gameObject.GetPhotonView().Controller.NickName;
         foreach (var player in PhotonNetwork.CurrentRoom.Players)
         {
             string playerName = player.Value.NickName;
-            if(name.Equals(playerName))
+            if (name.Equals(playerName))
             {
                 UpdatePlayerScore(playerName);
+                rankText.text = "GameOver\nYour Score: " + playerScores[playerName];
                 count++;
                 PhotonNetwork.Destroy(other.gameObject);
             }
+
         }
+        
     }
+
     private void UpdatePlayerScore(string playerName)
     {
-        playerScores[playerName] =  count * 10;
+        playerScores[playerName] = count * 10;
         Debug.Log(playerName + " : " + playerScores[playerName] + "획득");
+        
+    }
+
+    private IEnumerator OutGame()
+    {
+        yield return new WaitForSecondsRealtime(5);
+        PhotonNetwork.LoadLevel("UI");
     }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
