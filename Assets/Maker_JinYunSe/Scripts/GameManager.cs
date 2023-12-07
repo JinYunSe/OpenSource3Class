5888735349564dfb3ad3fa02acf1b7f3006c5c2b
JinYunSe <wlsdbstp1234@gmail.com>
2023-12-07 11:54:42 +0900

    UI 약간 수정 및 꼬리 잡기 Timer UI 구현 중

--- Assets/Maker_JinYunSe/Scripts/GameManager.cs
+++ Assets/Maker_JinYunSe/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using Photon.Realtime;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text.RegularExpressions;
 using Unity.VisualScripting;
@@ -14,16 +15,26 @@ public class GameManager : MonoBehaviour
 {
     GameObject[] player;
     PhotonView[] photonView;
+    Stopwatch watch = new Stopwatch();
+    bool timeOut = false;
+    float timer = 3600; 
     void Start()
     {
+        StartCoroutine(UserFind());
+        watch.Start();
         StartCoroutine(TimeOutEndGame());
+    }
+
+    public IEnumerator UserFind()
+    {
+        yield return null;
+        player = GameObject.FindGameObjectsWithTag("Player");
+        photonView = new PhotonView[player.Length];
     }
 
     public IEnumerator TimeOutEndGame()
     {
-        yield return new WaitForSecondsRealtime(60);
-        player = GameObject.FindGameObjectsWithTag("Player");
-        photonView = new PhotonView[player.Length];
+        yield return new WaitUntil(()=>(timeOut == true));
         for (int i = 0; i < player.Length; i++)
         {
             photonView[i] = player[i].GetPhotonView();
@@ -31,7 +42,7 @@ public class GameManager : MonoBehaviour
             int time = int.Parse(Regex.Replace(timetemp, @"\D", string.Empty));
             Transform endGamePanel = player[i].transform.Find("EndGameCanvas/EndGameUI");
             endGamePanel.Find("TimeText").GetComponent<Text>().text = "Time : " + time;
-            bool GetTailCheck =  player[i].transform.Find("Tails").gameObject.activeSelf;
+            bool GetTailCheck = player[i].transform.Find("Tails").gameObject.activeSelf;
             if (GetTailCheck) endGamePanel.Find("WinLoseText").GetComponent<Text>().text = "You Win!!";
             else endGamePanel.Find("WinLoseText").GetComponent<Text>().text = "You Lose...";
             if (photonView[i].IsMine) endGamePanel.parent.gameObject.SetActive(true);
